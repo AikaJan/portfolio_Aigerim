f92713e38c34b46ca8acfbe38a9921d86f27a750
Aigerim Dzhanyshpekova <107724350+AikaJan@users.noreply.github.com>
2024-07-16 14:45:27 -0700

    7/16/24
    added new projects and changed the design

---
+++ App.js
@@ -0,0 +1,40 @@
+import "./styles/main.css";
+import "./styles/reset.css";
+import Navbar from "./components/navbar/Navbar.js";
+import Footer from "./components/footer/Footer.js";
+import Home from "./pages/Home.js";
+import Projects from "./pages/Projects.js";
+import Contacts from "./pages/Contacts.js";
+import SelfProject from "./pages/SelfProject.js";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ScrollToTop from "./utils/skrollToTop.js";
+import Repos from "./components/repos/Repos.js";
+
+function App() {
+  const username = "AikaJan";
+  const token =
+    "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";
+
+  return (
+    <div className="App">
+      <Router>
+        <ScrollToTop />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/self-project/:id" element={<SelfProject />} />
+          <Route path="/contacts" element={<Contacts />} />
+          {/* <Route
+            path="/repos"
+            element={<Repos username={username} token={token} />}
+          /> */}
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
